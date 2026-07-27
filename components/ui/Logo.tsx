7f4d4ea02501c3184ccc5f833025c5872697573b
kelosocialeu <kelosocial.eu@gmail.com>
2026-07-27 18:24:29 +0200

    Update Logo.tsx

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,9 +1,7 @@
-export default function Logo() {
-  return (
-    <img
-      src="https://kelosocial.sirv.com/logo.png"
-      alt="Kelo Social"
-      className="h-14 w-auto"
-    />
-  );
+interface LogoProps {
+  className?: string;
+}
+
+export default function Logo({ className = "h-14 w-auto" }: LogoProps) {
+  return <img src="https://kelosocial.sirv.com/logo.png" alt="Kelo Social" className={className} />;
 }
