b2d01f347c61e42a07fad6dea7d4b0e4d47e30c2
kelosocialeu <kelosocial.eu@gmail.com>
2026-07-27 23:32:20 +0200

    Update auth.ts

--- types/auth.ts
+++ types/auth.ts
@@ -1,3 +1,23 @@
+export interface AtpSession {
+  accessJwt: string;
+  refreshJwt: string;
+  handle: string;
+  did: string;
+  pdsUrl: string;
+}
+
+export interface LoginCredentials {
+  identifier: string;
+  password: string;
+  pdsUrl: string;
+}
+
+export interface PdsProvider {
+  id: string;
+  label: string;
+  url: string;
+}
+
 export interface SignupPayload {
   handle: string;
   email: string;
