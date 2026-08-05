c1d0549c35832f89e8596757cb65e7220aa1669d
kelosocialeu <kelosocial.eu@gmail.com>
2026-08-05 23:32:32 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,17 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 import MobileNavigationShell from "@/components/layout/MobileNavigationShell";
+import {
+  AuthProvider,
+} from "@/components/providers/AuthProvider";
 
 export const metadata: Metadata = {
   title: "Kelo Social",
   description:
     "Le réseau social décentralisé propulsé par AT Protocol",
   icons: {
-    icon: "https://kelosocial.sirv.com/logo.png",
+    icon:
+      "https://kelosocial.sirv.com/logo.png",
     shortcut:
       "https://kelosocial.sirv.com/logo.png",
     apple:
@@ -24,9 +28,11 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className="bg-[#faf9f6] font-sans text-gray-900 antialiased">
-        <MobileNavigationShell>
-          {children}
-        </MobileNavigationShell>
+        <AuthProvider>
+          <MobileNavigationShell>
+            {children}
+          </MobileNavigationShell>
+        </AuthProvider>
       </body>
     </html>
   );
