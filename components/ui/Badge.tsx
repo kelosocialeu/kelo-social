a72ea038f2486fd74f64895f03a0e403ea72517d
kelosocialeu <kelosocial.eu@gmail.com>
2026-09-26 16:20:13 +0200

    Adapt certification badges to selected theme palette

--- components/ui/Badge.tsx
+++ components/ui/Badge.tsx
@@ -31,9 +31,9 @@ function CertifiedBadge({ size }: { size: number }) {
           x2="88%"
           y2="92%"
         >
-          <stop offset="0%" stopColor="#55B8FF" />
-          <stop offset="52%" stopColor="#6578F5" />
-          <stop offset="100%" stopColor="#B347FF" />
+          <stop offset="0%" stopColor="var(--primary)" />
+          <stop offset="52%" stopColor="var(--secondary)" />
+          <stop offset="100%" stopColor="var(--primary)" />
         </linearGradient>
       </defs>
       <circle cx="256" cy="256" r="187" fill="url(#kelo-certified-gradient)" />
@@ -68,11 +68,11 @@ function TrustedVerifierBadge({ size }: { size: number }) {
           x2="88%"
           y2="92%"
         >
-          <stop offset="0%" stopColor="#48B8FF" />
-          <stop offset="32%" stopColor="#5F92F7" />
-          <stop offset="61%" stopColor="#746CF3" />
-          <stop offset="82%" stopColor="#934DEB" />
-          <stop offset="100%" stopColor="#B23BE6" />
+          <stop offset="0%" stopColor="var(--primary)" />
+          <stop offset="32%" stopColor="var(--primary)" />
+          <stop offset="61%" stopColor="var(--secondary)" />
+          <stop offset="82%" stopColor="var(--secondary)" />
+          <stop offset="100%" stopColor="var(--primary)" />
         </linearGradient>
       </defs>
       <path d="M 211 70 L 196 79 L 173 103 L 166 108 L 146 116 L 124 117 L 106 121 L 90 130 L 77 143 L 67 162 L 64 178 L 65 195 L 68 207 L 67 229 L 59 248 L 47 264 L 42 274 L 38 288 L 39 313 L 45 329 L 55 343 L 65 352 L 88 365 L 102 379 L 109 391 L 115 413 L 121 425 L 137 443 L 146 449 L 166 456 L 181 457 L 198 454 L 221 444 L 231 442 L 244 442 L 257 445 L 271 452 L 284 456 L 309 456 L 330 448 L 339 442 L 347 434 L 353 426 L 359 414 L 363 397 L 373 378 L 389 363 L 412 350 L 425 336 L 432 323 L 436 309 L 436 288 L 434 279 L 428 265 L 415 248 L 407 228 L 406 211 L 410 191 L 409 168 L 405 156 L 395 140 L 387 132 L 375 124 L 362 119 L 334 117 L 316 112 L 299 101 L 281 81 L 273 75 L 261 69 L 244 65 L 230 65 Z" fill="url(#kelo-trusted-verifier-gradient)" />
